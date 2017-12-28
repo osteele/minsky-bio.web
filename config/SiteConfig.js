8fb1b69e57a6659c4b8626c3ae9dcf91121d9e50
Oliver Steele <steele@osteele.com>
2017-12-28 12:30:35 -0500

    Update website name

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
 	pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-	siteTitle: 'Margaret Minsky', // Navigation and Site Title
-	siteTitleAlt: 'Writing in progress', // Alternative Site title for SEO
-	siteUrl: 'https://margaret-minsky.netlify.com', // Domain of your site. No trailing slash!
+	siteTitle: 'Marvin Minsky', // Navigation and Site Title
+	siteTitleAlt: 'Essays by and about Marvin Minsky', // Alternative Site title for SEO
+	siteUrl: 'https://mminsky.netlify.com', // Domain of your site. No trailing slash!
 	siteLanguage: 'en', // Language Tag on <html> element
 	siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
 	siteDescription: 'Essays',
@@ -12,9 +12,7 @@ module.exports = {
 	ogSiteName: 'margaret.minsky', // Facebook Site Name
 	// googleAnalyticsID: 'UA-12345689-1',
 	copyright: 'Copyright © 2017 by Margaret Minsky. All rights reserved.',
-	client: 'Client',
 	date: 'Date',
-	service: 'Service',
 	// Date format used in your project header
 	// More information here: https://date-fns.org/v1.29.0/docs/format
 	dateFormat: 'DD.MM.YYYY',
